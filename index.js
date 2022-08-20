2d469e1ad0ffa0529deaa956a6fa1f8616b80f46
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-08-20 14:42:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -297,7 +297,7 @@ bot.start(async ctx => {
 	await ctx.replyWithMarkdown(
 		...getGreetMessage({
 			botUsername: ctx.botInfo.username,
-			isGroup: ctx.update.message.chat.id < 0,
+			isGroup: null,
 		})
 	)
 })
