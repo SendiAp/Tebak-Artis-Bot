de78830a3aca61d39981d6ac0fe70d84e0308854
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-08-20 14:28:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const getAddToGroupButton = botUsername => ({
 		],
 	},
 })
-const getGreetMessage = ({botUsername, isGroup.null}) => [
+const getGreetMessage = isGroup ? null ({botUsername, isGroup}) => [
 	trueTrim(`
 	__Halo everyone,saya adalah bot tebak umur artis__  •tambahkan saya kegrub dan semua perintah akan berfungsi•
 
