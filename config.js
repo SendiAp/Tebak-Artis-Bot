330ebeeae4b95324ce4777f7154c4fccc3e0b471
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-04-18 11:26:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	token: "6317646148:AAEfkzXdFdmUDLnr5hxUrhskmvO6MSUwuyk",
+	token: "7054978187:AAEwXYfMKr_TpFIciCfhopx3QNsEGx3KR44",
 	rounds: 5, //rounds amount
 	waitDelay: 12000, //1 round duration
 	timerSteps: 10, //timer indicators
